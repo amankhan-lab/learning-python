c90403f530aff4d826b55c6eba825489fd39bb3e
amankhan-lab <romanreings93513@gmail.com>
2025-08-28 19:26:32 +0530

    Add files via upload
    first file is to know about how to write code in python from scratch and second one for the basic understanding of variables

---
+++ python project 1.py
@@ -0,0 +1,9 @@
+# ABOUT MY LIFE
+print("i like biryani")
+print("i want to live")
+print("ia am amankhan altafkhan pathan")
+print("my dad is great")
+print("my mom is great")
+print("my dream is big")
+print("i will do anything and everything to achieve my dream")
+print("i will make my parents proud no matter what")
---
+++ pythonproject2.py
@@ -0,0 +1,151 @@
+# VARIABLES : A CONTAINER FOR A VALUE (strig, integer, float, boolean)
+# A VARIABLE BEHAVES AS IF IT WAS THE VALUE IT CONTAINS
+
+# string 
+first_name = "bro"
+print(first_name)
+
+print(f"hello {first_name}")
+
+food = "pizza"
+
+print(f"you like {food}")
+
+
+email = "bro123@fake.com"
+
+print(f"your email is: {email}")
+
+
+marksheet = "fail"
+print(marksheet)
+
+print(f"your school reult is {marksheet}")
+
+
+university = "Stanford University"
+print(university)
+print(f"you got selected into {university}")
+
+
+variable = "college"
+print(variable)
+print(f"you wanna go to {variable},don't you?")
+
+
+illness = "disease"
+print(illness)
+print(f"bro you got a {illness}?")
+
+
+
+# Integers
+
+age = 25 
+print(age)
+print(f"you are {age} years old")
+
+
+quantity = 3
+print(quantity)
+print(f"you are buying {quantity} items")
+
+
+num_of_students = 30 
+print(num_of_students)
+print(f"you have {num_of_students} students in your class")
+
+family = 6
+print(family)
+print(f"we are the family of {family}")
+
+
+
+# Float
+
+
+grade_in_10th = 73
+grade_in_12th = 58.50
+print(grade_in_10th,grade_in_12th)
+print(f"my percentage in 10th standard is: {grade_in_10th} and my percentage in 12th standard is: {grade_in_12th}")
+
+
+price = 10.99
+print(price)
+print(f"the price is {price}")
+
+
+gpa = 3.2
+print(gpa)
+print(f"your gpa ia: {gpa}")
+
+
+distance = 5.5
+print(distance)
+print(f"you ran {distance}km")
+
+
+height = 5.8
+print(height)
+print(f"my height is {height}")
+
+
+weight = 54.5 
+print(weight)
+print(f"my weight is {weight}")
+
+
+# Boolean
+
+
+is_student = True
+print(f"you are a student: {is_student}")
+
+is_student = True
+if is_student:
+    print("you are a student")
+else:
+    print("you are NOT a student")
+
+
+for_sale = True
+if for_sale:
+    print("the item is for sale")
+else:
+    print("the item is NOT available")
+
+
+is_online = True
+if is_online:
+    print("you are online")
+else:
+    print("you are offline")
+
+
+
+result = True
+if result:
+    print("you are pass")
+else:
+    print("you are fail")
+
+
+is_asus = False
+if is_asus:
+    print("asus is the best company")
+else:
+    print("asus is not the best company")
+
+
+is_match = False
+if is_match:
+    print("i won the match")
+else:
+    print("i lose the match")
+
+
+is_job = False
+if is_job:
+    print("i got fired")
+else:
+    print("i got a promotion")
