ef4a39aed01811636fb12a12e0807ef40a86066e
amankhan-lab <romanreings93513@gmail.com>
2025-10-21 00:00:15 +0530

    Add files via upload

---
+++ Circumference_of_a_circle.py
@@ -0,0 +1,10 @@
+# arithematic math operator
+import math
+
+# circumferrance of a circle is 2 * pi * r
+
+Radius = float(input("Enter the radius of a circle: "))
+
+circumference = 2 * math.pi * Radius
+
+print(f"The circumference of a circle is: {math.floor(circumference)}cm")
