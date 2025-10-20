7348783aeadf934c7ad0f1fe66a7fb2dab8514b9
amankhan-lab <romanreings93513@gmail.com>
2025-10-20 16:32:27 +0530

    Add files via upload
    it is a shopping cart program

--- input_output.py
+++ input_output.py
@@ -1,27 +1,9 @@
+#SHOPPING CART PROGRAM
 
-Name = input("Please Enter your name:")
-age = int(input(f"Please Enter your age:"))
-print(f"Hello {Name}")
-age = age + 1
-print(f"Your age is {age}")
+item = input("enter your order here: ")
+price = float(input("Enter the price of an irtem you bought: "))
+quantity = int(input("enter the quantity of an item: "))
 
-mass = float(input("Enter your mass: "))
-g = 9.81
-weight = mass * g
-print(f"{Name}, Your Weight on Earth is {round (weight, 2)}")
+total_cost = quantity * price
 
-#KINETIC ENERGY OF A MATERIAL
-
-mass = float(input("please Enter your mass:"))
-velocity = float(input("please enter your velocity:"))
-kinetic_energy = 0.5 * mass / velocity ** 2
-print(f"your kinetic energy is: {round (kinetic_energy, 2)}")
-
-#POTENTIAL ENERGY OF AN OBJECT
-
-mass = int(input("please enter the mass of the object:"))
-g = 9.8
-height = int(input("please enter the height of the object:"))
-
-potential_energy = mass * g * height
-print(f"Your Potential energy is: {round(potential_energy, 2)}")
+print(f"you bought {quantity}x {item}, your tootal cost is ${round(total_cost, 2)}")
