72449c829af06ab8fcd9a4aec3fc8a3d106e1c7b
amankhan-lab <romanreings93513@gmail.com>
2025-10-21 00:07:54 +0530

    Add files via upload

---
+++ Area_of_a_circle.py
@@ -0,0 +1,10 @@
+# arithematic math operator
+import math
+
+# AREA OF A CIRCLE : pi r^2
+
+radius = float(input("Enter the radius of a circle: "))
+
+Area = math.pi * radius ** 2
+
+print(f"Area of the circle is: {round(Area, 2)}cm")
