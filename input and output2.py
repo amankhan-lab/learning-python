a7eb398c81903133fa7413083e4df23d4344a994
amankhan-lab <romanreings93513@gmail.com>
2025-10-20 22:54:19 +0530

    Add files via upload
    madlibs story telling that you people will enjoy i hope

---
+++ input and output2.py
@@ -0,0 +1,15 @@
+#MADLIBS GAME
+adjective1 = input("Enter the adjective: ")
+adjective2 = input("Enter the adjective: ")
+adjective3 = input("Enter the adjective: ")
+adjective4 = input("Enter the adjective: ")
+adjective5 = input("Enter the adjective: ")
+noun1 = input("Enter noun: ")
+noun2 = input("Enter noun:")
+
+print(f"once upon a time, there is a {adjective1} king in a village called {noun1}")
+print(f"The has a very high temper and was called {adjective2} in his kingdom")
+print(f"""he has a queen named {noun2} she is a very {adjective3} soul and she always try to make him
+       understand to stop all the {adjective4}""")
+print(f"""king eventually fell in {adjective5} with queen, and started listening to her every command that way kingdom 
+      of {noun1} is developed and become great""")
