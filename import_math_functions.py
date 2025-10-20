f1cbb6c179c3027fc3ebf75d1d04867abe458e33
amankhan-lab <romanreings93513@gmail.com>
2025-10-20 23:48:25 +0530

    Add files via upload
    some important arithematic functions by importing math

---
+++ import_math_functions.py
@@ -0,0 +1,13 @@
+# arithematic math operator
+# some important functions
+
+import math
+print(math.pi)
+print(math.e)
+x = 25.9
+# result = math.sqrt(x)
+# result = math.ceil(x) # ceil is a function to round off the value by increasing is ffloating point number 
+result = math.floor(x)
+print(result)
+
+
