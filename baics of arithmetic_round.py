f8dc81c92f42a99fdbb6a79526e6e511833414fe
amankhan-lab <romanreings93513@gmail.com>
2025-10-21 16:46:28 +0530

    Add files via upload

---
+++ baics of arithmetic_round.py
@@ -0,0 +1,10 @@
+# arithematic math operator
+import math
+
+# round function: to round-off the value into decimal places and its upto us to decide how much decimal places
+#  we want to add
+
+x = float(input("Enter the number: "))
+
+# print(f"your round value is {round(x, 2)}")
+print(f"your round value is {round(x, 1)}")
