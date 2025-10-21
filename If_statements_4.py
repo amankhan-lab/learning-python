9fe17da8896ef192be0275f70ed6112bfd278572
amankhan-lab <romanreings93513@gmail.com>
2025-10-22 00:17:24 +0530

    Add files via upload

---
+++ If_statements_4.py
@@ -0,0 +1,15 @@
+# IF statement: Do some code IF condition is true else do something
+
+# Use of booleans
+
+am_going = False
+if am_going:
+    print("i am going on a vacation trip with friends")
+else:
+    print("Sorry guys abba nahi maane!")
+
+eligible = True
+if eligible:
+    print("You are eligible for MAH MCA CET entrance exam")
+else:
+    print("Sorry you are not eligible for MAH MCA CE entrance exam!")
