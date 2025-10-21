f25b7cdecb0cd55c904364d332920e55ab28e70e
amankhan-lab <romanreings93513@gmail.com>
2025-10-21 20:39:13 +0530

    Add files via upload

---
+++ baics of arithmetic_discount_price.py
@@ -0,0 +1,12 @@
+# arithematic math operator
+import math
+
+# Finding the Discount price
+
+item = input("Enter the name of the item: ")
+
+price = float(input("Enter the price here: "))
+
+discount = 12.5 / 100 * price
+
+print(f"Your discount of a {item} is: {discount}")
